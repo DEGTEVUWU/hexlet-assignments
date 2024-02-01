d72efb32527b4362c67ac92debb67a030fd5111e
@hexlet/cli <support@hexlet.io>
2024-02-01 10:51:35 +0200

    submit java-oop-ru/patterns_01.02.24_10.43.56

---
+++ java-oop-ru/patterns_01.02.24_10.43.56/src/main/java/exercise/InputTag.java
@@ -0,0 +1,18 @@
+package exercise;
+
+public class InputTag implements TagInterface {
+    private String first;
+    private String second;
+    InputTag(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+    @Override
+    public String render() {
+        return "<input type=" + "\"" + first + "\"" + " value=" + "\"" + second + "\"" + ">";
+    }
+    @Override
+    public String toString() {
+        return "<input type=" + "\"" + first + "\"" + " value=" + "\"" + second + "\"" + ">";
+    }
+}
