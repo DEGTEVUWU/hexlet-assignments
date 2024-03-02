ae5d5cf2bcadcd90aa75772c11480437bf84ff67
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-03-02 15:23:20 +0200

    fix

--- java-web-ru/template/src/main/java/exercise/App.java
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -15,11 +15,11 @@ public final class App {
 
     // Каждый пользователь представлен объектом класса User
     private static final List<User> USERS = Data.getUsers();
-
+    private static List<User> selectUsers = USERS.stream()
+            .sorted(Comparator.comparing(User::getId))
+            .collect(Collectors.toList());
     public static Javalin getApp() {
-//        var selectUsers = USERS.stream()
-//                .sorted(Comparator.comparing(User::getId))
-//                .collect(Collectors.toList());
+
 
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
@@ -28,9 +28,7 @@ public final class App {
         // BEGIN
         app.get("/users", ctx -> {
             var header = "Список всех юзеров";
-            var selectUsers = USERS.stream()
-                    .sorted(Comparator.comparing(User::getId))
-                    .collect(Collectors.toList());
+
 
             UsersPage page = new UsersPage(selectUsers, header);
             ctx.render("users/index.jte", Collections.singletonMap("page", page));
@@ -43,10 +41,10 @@ public final class App {
             if (userNumber > USERS.size() || userNumber < 0) {
                 throw new NotFoundResponse("Company not found");
             }
-            var id = USERS.get(userNumber).getId();
-            var first = USERS.get(userNumber).getFirstName();
-            var last = USERS.get(userNumber).getLastName();
-            var email = USERS.get(userNumber).getEmail();
+            var id = selectUsers.get(userNumber - 1).getId();
+            var first = selectUsers.get(userNumber - 1).getFirstName();
+            var last = selectUsers.get(userNumber - 1).getLastName();
+            var email = selectUsers.get(userNumber -1).getEmail();
             var resultUser = new User(id, first, last, email);
 
             UserPage page = new UserPage(resultUser, header);
