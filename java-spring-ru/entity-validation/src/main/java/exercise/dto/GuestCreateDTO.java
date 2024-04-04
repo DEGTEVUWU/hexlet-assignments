8875d66718ea6624f9f24477ba3687f9ff004dd2
@hexlet/cli <support@hexlet.io>
2024-04-04 09:55:12 +0300

    submit java-spring-ru/entity-validation

--- java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
+++ java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -1,6 +1,9 @@
 package exercise.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
