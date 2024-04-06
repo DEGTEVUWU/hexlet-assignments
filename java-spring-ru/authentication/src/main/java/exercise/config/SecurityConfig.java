b8dcbd48331c67654bb1b5f5dcbcb8e4c5991c62
@hexlet/cli <support@hexlet.io>
2024-04-06 18:01:30 +0300

    submit java-spring-ru/authentication

--- java-spring-ru/authentication/src/main/java/exercise/config/SecurityConfig.java
+++ java-spring-ru/authentication/src/main/java/exercise/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package exercise.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
--- java-spring-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
+++ java-spring-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
@@ -3,7 +3,6 @@ package exercise.service;
 import exercise.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
